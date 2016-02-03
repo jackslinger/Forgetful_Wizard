35a11745e675e3fdd172da315a0a3d632d0a05d6
Jack Slinger <jackslinger10@gmail.com>
2016-02-03 10:21:55 +0000

    Started adding a menu method to allow inventory ect.

--- main.py
+++ main.py
@@ -38,6 +38,35 @@ class Message:
 			#Add the line to the message buffer
 			self.buffer.append(line)
 
+def menu(header, options, width):
+	if len(options) > 26:
+		raise ValueError("Can't have a menu with more than 26 options.")
+
+	#Calculate the word wrapped height of the header and the total window height
+	header_height = libtcod.console_get_height_rect(con, 0, 0, width, SCREEN_HEIGHT, header)
+	height = len(options) + header_height
+
+	#Create a new window for the menu
+	window = libtcod.console_new(width, height)
+
+	#Print the header with auto wrap
+	libtcod.console_set_default_foreground(window, libtcod.white)
+	libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
+
+	#Print all the options
+	y = header_height
+	letter_index = ord('a')
+	for option_text in options:
+		text = "(" + chr(letter_index) + ") " + option_text
+		libtcod.console_print_ex(window, 0, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
+		y += 1
+		letter_index += 1
+
+	#Blit to the middle of the main console
+	x = SCREEN_WIDTH/2 - width/2
+	y = SCREEN_HEIGHT/2 - height/2
+	libtcod.console_blit(window, 0, 0, width, height, 0, x, y, 1.0, 0.7)
+
 def handle_keys(board):
 	game_state = board.game.game_state
 	player = board.player
@@ -127,6 +156,7 @@ while not libtcod.console_is_window_closed():
 
 	libtcod.console_blit(con, 0, 0, MAP_WIDTH, MAP_HEIGHT, 0, 0, 0)
 	libtcod.console_blit(panel, 0, 0, SCREEN_WIDTH, PANEL_HEIGHT, 0, 0, PANEL_Y)
+	menu("Test header.", ["one","two","three"], 50)
 	libtcod.console_flush()
 
 	player_action = handle_keys(board)
